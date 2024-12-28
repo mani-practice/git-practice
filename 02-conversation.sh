44605cf1c3866d0c479dea63c395ef1ec0e0886b
mani-practice <maniworldian7@gmail.com>
2024-12-28 07:59:31 +0530

    I created conversation btw ramesh and suresh

--- 02-conversation.sh
+++ 02-conversation.sh
@@ -1,1 +1,6 @@
 #!/bin/bash
+
+echo "suresh:: Hi ramesh, How are you?"
+echo "ramesh:: Hi sursh, I am going good.How about you?"
+echo "suresh:: I am doing good ramesh.what's going on?"
+echo "ramesh:: I started learning shell scripting"
