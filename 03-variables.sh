b6f5aa1f44574d6073cabab1138ab0b2bf5498e5
mani-practice <maniworldian7@gmail.com>
2024-12-28 08:09:39 +0530

    variables concept

---
+++ 03-variables.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+PERSON1=ramesh
+PERSON2=suresh
+
+echo "${PERSON2}:: Hi ${PERSON1}, How are you?"
+echo "${PERSON1}:: Hi ${PERSON2}, I am going good.How about you?"
+echo "${PERSON2}:: I am doing good ${PERSON1}.what's going on?"
+echo "${PERSON1}:: I started learning shell scripting"
