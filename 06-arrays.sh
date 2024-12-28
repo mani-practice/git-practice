a9c877d4a6f430f721be29efa47e61b18fca30ef
mani-practice <maniworldian7@gmail.com>
2024-12-28 14:31:00 +0530

    args

---
+++ 06-arrays.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+FRUITS=("Apple" "Banana" "Orangr")
+
+echo "first fruit is:=${FRUITS[0]}"
+echo "second fruit is:=${FRUITS[1]}"
+echo "second fruit is:=${FRUITS[2]}"
+
+echo "All fruits are:=${FRUITS[@]}"
