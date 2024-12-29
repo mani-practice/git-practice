0e015f1645c8bbbb7be547eed69b6a4313f9d06a
mani-practice <maniworldian7@gmail.com>
2024-12-30 01:06:23 +0530

    data types

--- 10-conditions.sh
+++ 10-conditions.sh
@@ -2,7 +2,7 @@
 
 NUMBER=$1
 
-if [ $NUMBER -gt 20 ] 
+if [ $NUMBER -gt 20 ] # -gt=greater than like lt, eq and -ne means not equal to and -ge means greater than or equal to
 then
 
 echo "Given number is: $NUMBER is greater than 20"
