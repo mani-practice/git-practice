b20ae60dca176a1016442d85866713b3057bf8dc
mani-practice <maniworldian7@gmail.com>
2024-12-30 11:34:37 +0530

    data types

--- 11-conditions.sh
+++ 11-conditions.sh
@@ -4,10 +4,10 @@
   HAPPYSOULS2=$2
 
 
-LOVE=$(($HAPPY SOULS1+HAPPY SOULS2))
+SUM=$(($HAPPY SOULS1+HAPPY SOULS2))
 
-echo "The combination of $HAPPYSOULS1 and $HAPPYSOULS2 is: $LOVE"
+echo "The combination of $HAPPYSOULS1 and $HAPPYSOULS2 is: $SUM"
 
-echo "$LOVE are happy people"
+echo "$SUM are happy people"
 
 
