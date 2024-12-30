6e7217ffb90ffcaa1d76c5e883970895ee1dcce0
mani-practice <maniworldian7@gmail.com>
2024-12-30 11:55:32 +0530

    data types

--- 11-conditions.sh
+++ 11-conditions.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
- HAPPYSOUL=$("MANI" "SHREE")
+ FRUITS=("Apple" "Banana" "Orange")
 
- echo "first HAPPYSOULS  is:=${HAPPYSOUL[0]}"
- echo "second HAPPYSOULS is:=${HAPPYSOUL[1]}"
+echo "first fruit is:=${FRUITS[0]}"
+echo "second fruit is:=${FRUITS[1]}"
+echo "second fruit is:=${FRUITS[2]}"
 
-echo "The All happy souls are:=${$HAPPYSOUL[@]}"
+echo "All fruits are:=${FRUITS[@]}
 
 
 
