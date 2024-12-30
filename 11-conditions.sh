51868aa5737ce025e2e3eae4203620a308fa7824
mani-practice <maniworldian7@gmail.com>
2024-12-30 11:57:15 +0530

    data types

--- 11-conditions.sh
+++ 11-conditions.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
- FRUITS=("Apple" "Banana" "Orange")
+ COUPLES=("Apple" "Banana" "Orange")
 
-echo "first fruit is:=${FRUITS[0]}"
-echo "second fruit is:=${FRUITS[1]}"
-echo "second fruit is:=${FRUITS[2]}"
+echo "first fruit is:=${COUPLES[0]}"
+echo "second fruit is:=${COUPLES[1]}"
+echo "second fruit is:=${COUPLES[2]}"
 
-echo "All fruits are:=${FRUITS[@]}"
+echo "All fruits are:=${COUPLES[@]}"
 
 
 
