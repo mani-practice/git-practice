63fd6cbe6237866ea78490f1c08aa213979b8634
mani-practice <maniworldian7@gmail.com>
2024-12-30 11:56:22 +0530

    data types

--- 11-conditions.sh
+++ 11-conditions.sh
@@ -6,7 +6,7 @@ echo "first fruit is:=${FRUITS[0]}"
 echo "second fruit is:=${FRUITS[1]}"
 echo "second fruit is:=${FRUITS[2]}"
 
-echo "All fruits are:=${FRUITS[@]}
+echo "All fruits are:=${FRUITS[@]}"
 
 
 
