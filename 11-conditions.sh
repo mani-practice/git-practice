ac24018bc767055082f0774d1c035b97bc8cbf6a
mani-practice <maniworldian7@gmail.com>
2024-12-30 12:12:03 +0530

    data types

--- 11-conditions.sh
+++ 11-conditions.sh
@@ -2,13 +2,13 @@
 
  LOVE=$1
 
- if [ $LOVE -gt 12]
+ if [ $LOVE -gt 12 ]
  then
 
  echo "Bhagyashree love mani: $LOVE is greater than 12"
 
  else
- 
+
  echo "mani loves Bhagyshree: $LOVE is les than 12"
 
  fi
